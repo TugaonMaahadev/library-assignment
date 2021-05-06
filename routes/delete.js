1d45d976272247a33ac9d6ac5bab4a951d1878d3
TugaonMaahadev <maahadev.1si14cs411@gmail.com>
2021-05-06 17:00:23 +0530

    notes added

--- routes/delete.js
+++ routes/delete.js
@@ -6,7 +6,7 @@ module.exports={
     deleteBook:(req,res)=>{
         let {id,role}=req.body;
         if(role!=='admin'){
-            res.send({Message:"You are not authorized to create user"})
+            res.send({Message:"You are not authorized to delete book"})
         }else{
         dao.deleteBook(id).then((data)=>{
             res.send({Message:"Book has been deleted successfuly",status:200})
@@ -18,7 +18,7 @@ module.exports={
 deleteUser:(req,res)=>{
     let {id,role}=req.body;
     if(role!=='admin'){
-        res.send({Message:"You are not authorized to create user"})
+        res.send({Message:"You are not authorized to delete user"})
     }else{
     dao.deleteUser(id).then((data)=>{
         res.send({Message:"User has been deleted successfuly",status:200})
--- routes/update.js
+++ routes/update.js
@@ -6,7 +6,7 @@ module.exports={
     updateBook:(req,res)=>{
         let {id,bookName,author,status,user,email,role}=req.body;
         if(role!=='admin'){
-            res.send({Message:"You are not authorized to create user"})
+            res.send({Message:"You are not authorized to update book"})
         }else{
         dao.updateBook(id,bookName,author,status,user,email).then((data)=>{
             res.send({Message:"Book updated successfuly",status:200})
@@ -18,7 +18,7 @@ module.exports={
     updateUser:(req,res)=>{
         let {name,phone,email,role}=req.body;
         if(role!=='admin'){
-            res.send({Message:"You are not authorized to create user"})
+            res.send({Message:"You are not authorized to update user"})
         }else{
             dao.updateUser(name,phone,email).then((data)=>{
                 res.send({Message:"Book updated successfuly",status:200})
